9a54ede3385eaa4a26367db12fe7da24c58508c8
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-12-14 07:13:04 +0530

    Created variabels and const values :)

---
+++ game.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import random
+
+#Hello there im Manuja A.K.A anonixli --im creating this using python
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50 #HELLO IM JUST ASSUMING THIS IS 50 FOR NOW
+SPACE_SIZE = 50
+BODY_PART_START = 3
+SNAKE_COLOR = "#00f5c0" #   this is for my first version
+FOOD_COLOR = "#FF0000" # RED FOR FOOD 
+BG_COLOR = "#000000" #BLACK hmm... yh ig mybe ill create a light version as well... 0w0
+
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direct(new_direct):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+
+window = Tk()
